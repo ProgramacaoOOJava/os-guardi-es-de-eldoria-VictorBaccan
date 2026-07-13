04810670edbc49986a35f2632045fae117a29d73
Victor M. Baccan <124940939+VictorBaccan@users.noreply.github.com>
2026-07-13 05:07:18 +0000

    DESAFIO NIVEL NOVATO COMPLETO

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
-
-    }
+	public static void main(String[] args) {
+//		INSTANCIAS DE CLASSES E EXIBIÇÃO DE STATUS
+		Personagem p1 = new Personagem("Akira", "Guerreiro", 15, 100, 137.8);
+		p1.exibirStatus();
+		
+		System.out.println();
+		
+		Personagem p2 = new Personagem("Satsuki", "Mago", 37, 78, 163.2);
+		p2.exibirStatus();
+	}
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,22 @@
 public class Personagem {
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
-
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
-
-    }
-
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
-    }
+	String nome;
+	String classe;
+	int nivel;
+	int vida;
+	double poderBase;
+	
+//	METÓDO DE EXIBIÇÃO DE STATUS DE PERSONAGEM
+	public void exibirStatus() {
+		System.out.println("Nome: " + nome + "\nClasse: " + classe + "\nNivel: " + nivel + 
+				"\nVida: " + vida + "\nPoder Base: " + poderBase);
+	}
+	
+//	CRIAÇÃO DO CONSTRUTOR DA CLASSE PERSONAGEM
+	public Personagem(String nome, String classe, int nivel, int vida, double poderBase) {
+		this.nome = nome;
+		this.classe = classe;
+		this.nivel = nivel; 
+		this.vida = vida;
+		this.poderBase = poderBase;
+	}
 }
-
